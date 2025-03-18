830c87d45c2f7f6ff923048580715218cda260af
Lucas Oliveira Rodrigues <lucasrj58@gmail.com>
2025-03-18 20:55:00 -0300

    fix/retirando o comentario de explicação

--- cypress/e2e/carrinho.cy.js
+++ cypress/e2e/carrinho.cy.js
@@ -6,10 +6,10 @@ describe('Carrinho de Compras - SauceDemo', () => {
     it('Deve adicionar um produto ao carrinho e validar se ele está na listagem do carrinho', () => {
         const produtoNome = 'Sauce Labs Backpack';
 
-        // Adiciona o produto ao carrinho
+        
         cy.adicionarProdutoAoCarrinho(produtoNome);
 
-        // Valida se o produto aparece corretamente no carrinho
+        
         cy.validarProdutoNoCarrinho(produtoNome);
     });
 });
